3d5c19bb51ea65ca1a10820695585be56adb3ebe
tgsaifutdinov-source <tgsaifutdinov@gmail.com>
2026-09-23 04:17:25 +0600

    Build initial SchoolBook webmail interface

---
+++ app/layout.tsx
@@ -0,0 +1,3 @@
+import "./globals.css";
+export const metadata={title:"SchoolBook Mail",description:"Modern webmail interface"};
+export default function RootLayout({children}:{children:React.ReactNode}){return <html lang="ru"><body>{children}</body></html>}
